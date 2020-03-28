9413c2f8c000535b68da7bf3190b01dc79319ad1
Megan Lockette <megan3693@sbcglobal.net>
2020-03-28 09:53:16 -0700

    deploying to heroku

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Banner.css';
+
+const Banner = () => (
+  <div className="banner text-center d-flex align-items-center justify-content-center">
+    <p className="mb-5 pb-5">But don't click on any more than once!</p>
+  </div>
+);
+
+export default Banner;
